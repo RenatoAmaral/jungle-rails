b6a5b78bc97d67fd00f9a47728b28015bb986fd9
RenatoAmaral <srkimbo@gmail.com>
2018-03-30 00:02:41 +0000

    EMPTY CART BUG FIXED

--- app/controllers/admin/application_controller.rb
+++
@@ -1,3 +0,0 @@
-class Admin::ApplicationController < ApplicationController
-  http_basic_authenticate_with :name => Rails.configuration.http_auth[:user], :password => Rails.configuration.http_auth[:pass]
-end
